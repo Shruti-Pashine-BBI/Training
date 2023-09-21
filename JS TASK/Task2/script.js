ed63b4a83ee96b34fb2a086551ed0c5ce7165e4b
Shruti-Pashine-BBI <shruti.pashine@media4u.com>
2023-09-21 18:15:49 +0530

    Task 2

--- JS TASK/Task2/script.js
+++ JS TASK/Task2/script.js
@@ -1,23 +1,26 @@
-function stringManipulation(str) {
-  //reverse
-  const strnew = str.split("").reverse().join("");
-  console.log(strnew);
+class stringMan {
+  stringManipulation(str) {
+    //reverse
+    const strnew = str.split("").reverse().join("");
+    console.log(strnew);
 
-  //   console.log(str);
-  //uppercase
-  const strnew1 = strnew[0].toUpperCase() + strnew.substr(1);
-  console.log(strnew1);
-  //palindrome
-  if (strnew === str) {
-    console.log("Its Palindrome");
-  } else {
-    console.log("Not Palindrome");
+    //   console.log(str);
+    //uppercase
+    const strnew1 = strnew[0].toUpperCase() + strnew.substr(1);
+    console.log(strnew1);
+    //palindrome
+    if (strnew === str) {
+      console.log("Its Palindrome");
+    } else {
+      console.log("Not Palindrome");
+    }
+    //ount vowel
+    const count = str.match(/[aeiou]/gi).length;
+    console.log(count);
+    //Replace
+    const strnew2 = str.replaceAll("e", "X");
+    console.log(strnew2);
   }
-  //ount vowel
-  const count = str.match(/[aeiou]/gi).length;
-  console.log(count);
-  //Replace
-  const strnew2 = str.replaceAll("e", "X");
-  console.log(strnew2);
 }
-stringManipulation("Level");
+const obj = new stringMan();
+obj.stringManipulation("level");
